bfca982c5eae87039e024b9e10a6beb626f8a401
Suraj S <116282102+suraj0-11@users.noreply.github.com>
2023-03-25 20:40:37 +0530

    Create AppletP.java

---
+++ AppletP.java
@@ -0,0 +1,13 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+/*  
+<applet code="AppletP.class" width="300" height="300">  
+</applet>  
+*/
+public class AppletP extends Applet
+{
+    public void paint(Graphics g)
+    {
+        g.drawString("Welcome to applet", 100, 150);
+    }
+}
