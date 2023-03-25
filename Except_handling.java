2d76d54269b67fcd9de9c127a46f679cf1e78cb4
Suraj S <116282102+suraj0-11@users.noreply.github.com>
2023-03-25 19:56:00 +0530

    Create Except_handling.java

---
+++ Except_handling.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Tyrpp {
+    int c;
+    void div(int a, int b) {
+        try {
+            c = a / b;
+            System.out.println("Result=" + c);
+        } catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero");
+        }
+    }
+    public static void main(String args[]) {
+        Tyrpp obj = new Tyrpp();
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the values of a and b");
+        int no1 = in .nextInt();
+        int no2 = in .nextInt();
+        obj.div(no1, no2);
+    }
+}
